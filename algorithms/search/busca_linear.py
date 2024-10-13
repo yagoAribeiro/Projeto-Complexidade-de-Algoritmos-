3917c8b46aa5902ad1d3c146f04ce3430a6e9aa1
GabrielB03 <bielbomfim03@gmail.com>
2024-10-13 00:28:54 -0300

    Busca Linear e Bubble Sort implementados

--- algorithms/search/busca_linear.py
+++ algorithms/search/busca_linear.py
@@ -3,13 +3,21 @@ from algorithms.search.search_base import PlotableSearch
 class BuscaLinear(PlotableSearch):
     def __init__(self):
         super().__init__()
-    
+        
+    # Implementação da Busca Linear Iterativa
     def iterativeSearch(self, x, data=[]):
         for i in range(len(data)):
-            if (data[i] == x): return i
+            if data[i] == x:
+                return i
+        return -1
     
-    def recursiveSearch(self, x, data=[]):
-        return super().recursiveSearch(x, data)
+    # Implementação da Busca Linear Recursiva
+    def recursiveSearch(self, x, data=[], i=0):
+        if i >= len(data):
+            return -1
+        if data[i] == x:
+            return i
+        return self.recursiveSearch(x, data, i + 1)
     
-    def toString(self): 
+    def toString(self):
         return "Busca Linear"
