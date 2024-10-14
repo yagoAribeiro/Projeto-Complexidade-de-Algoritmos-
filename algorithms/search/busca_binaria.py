6ca7f3c85c4b3f636fcb2d97bcb46879585c12a1
Leandro-Oliveira-Bezilla <102204837+Leandro-Oliveira-Bezilla@users.noreply.github.com>
2024-10-14 18:19:53 -0300

    Update busca_binaria.py

--- algorithms/search/busca_binaria.py
+++ algorithms/search/busca_binaria.py
@@ -3,12 +3,41 @@ from algorithms.search.search_base import PlotableSearch
 class BuscaBinaria(PlotableSearch):
     def __init__(self):
         super().__init__()
-    
+
+    # Implementação da Busca Binaria Iterativa
     def iterativeSearch(self, x, data=[]):
-        return super().iterativeSearch(x, data)
+    inicio = 0
+    fim = len(arr) - 1
     
+    while inicio <= fim:
+        meio = (inicio + fim) // 2
+        
+        if arr[meio] == elemento:
+            return meio
+        
+        elif elemento < arr[meio]:
+            fim = meio - 1
+        
+        else:
+            inicio = meio + 1
+            
+    return -1
+
+    # Implementação da Busca Binaria Recursiva
     def recursiveSearch(self, x, data=[]):
-        return super().recursiveSearch(x, data)
+    if inicio > fim:
+        return -1
+    
+    meio = (inicio + fim) // 2    
+    
+    if arr[meio] == elemento:
+        return meio
+    
+    elif elemento < arr[meio]:
+        return busca_binaria(arr, elemento, inicio, meio - 1)
+    
+    else:
+        return busca_binaria(arr, elemento, meio + 1, fim)
     
     def toString(self):
         return "Busca Binária"
