16cf9cea1edb24dc6cd0cc960a0064d702e1bfaa
Leandro-Oliveira-Bezilla <102204837+Leandro-Oliveira-Bezilla@users.noreply.github.com>
2024-10-14 18:33:33 -0300

    Update selection_sort.py

--- algorithms/sort/selection_sort.py
+++ algorithms/sort/selection_sort.py
@@ -4,11 +4,32 @@ class SelectionSort(PlotableSort):
     def __init__(self):
         super().__init__()
 
+    # Implementação da Selection Sort Iterativo
     def iterativeSort(self, data=[]):
-        return super().iterativeSort(data)
+    n = len(arr)
     
+    for i in range(n):
+        menor_idx = i
+        
+        for j in range(i + 1, n):
+            if arr[j] < arr[menor_idx]:
+                menor_idx = j
+        
+        arr[i], arr[menor_idx] = arr[menor_idx], arr[i]
+
+    # Implementação da Selection Sort Recursivo
     def recursiveSort(self, data=[]):
-        return super().recursiveSort(data)
+    if i >= n - 1:
+        return
     
+    menor_idx = i
+    for j in range(i + 1, n):
+        if arr[j] < arr[menor_idx]:
+            menor_idx = j
+
+    arr[i], arr[menor_idx] = arr[menor_idx], arr[i]
+    
+    selection_sort_recursivo(arr, n, i + 1)
+
     def toString(self): 
         return "Método Selection Sort"
