3d607841468265931fdea08d960de348d14bb3db
YagoA <yagoamancio10@gmail.com>
2024-10-14 22:52:59 -0300

    [new] implementado busca largura corrigido

--- algorithms/search/busca_profundidade.py
+++ algorithms/search/busca_profundidade.py
@@ -5,10 +5,27 @@ class BuscaProfundidade(PlotableSearch):
         super().__init__()
     
     def iterativeSearch(self, x, data=[]):
-        return super().iterativeSearch(x, data)
+        visitados = []
+        for i in range(len(data)):
+            if x == i: return i
+            visitados.append(i)
+            for vizinho in data[i]:
+                if vizinho not in visitados:
+                    if x == vizinho: return vizinho
+                    visitados.append(vizinho)
+
     
-    def recursiveSearch(self, x, data=[]):
-        return super().recursiveSearch(x, data)
+    def recursiveSearch(self, x, data=[], visitados=[], i = 0):
+        return self.recursionSearch(x, data, [], i, len(data))
     
+    def recursionSearch(self, x, data=[], visitados=[], i = 0, limit = 0):
+        if i >= limit: return -1
+        if i not in visitados:
+            if i == x: return i
+            visitados.append(i)
+            self.recursionSearch(data[i], visitados, i+1, limit)
+        return -1
+
+
     def toString(self): 
         return "Busca por Profundidade"
